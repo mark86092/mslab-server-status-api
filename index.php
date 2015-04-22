6481c7ff24338b56bf182cf3000f835c04fc627a
Yen-chi Chen <mark86092@gmail.com>
2015-04-23 03:49:08 +0800

    Using Models

--- index.php
+++ index.php
@@ -18,28 +18,10 @@ $route->get('#^/repo/update#', function() {
 $route->get('#^/$#', function() {
 	try {
 		$conn  = DB::get();
-		$stmt = $conn->prepare("SELECT `host`, `cpu_cores`, `memory`, `load1`, `load5`, `load15`, `used_memory`, `last_updated` FROM `resources` WHERE `group`='mslab' ORDER BY `order`");
-		$stmt->execute();
-		$stmt->bind_result($host, $cpu_cores, $memory, $load1, $load5, $load15, $used_memory, $last_updated);
-		$data = array();
-		while ($stmt->fetch()) {
-			$data[] = array(
-				'host'=> $host,
-				'cpu_cores'=> $cpu_cores,
-				'memory' => (int)$memory,
-				'load1'=> (double)$load1,
-				'load5'=> (double)$load5,
-				'load15'=> (double)$load15,
-				'used_memory' => (int)$used_memory,
-				'last_updated'=> $last_updated
-			);
-		}
-		$stmt->close();
-		
+		$data = \Models\Resources::all($conn);
 		header("Access-Control-Allow-Origin: *");
 		header("Access-Control-Allow-Methods: *");
 		header("Content-Type: application/json");
-
 		echo json_encode($data);
 	} catch (Exception $e) {
 		http_response_code(500);
---
+++ libs/Models/Resources.php
@@ -0,0 +1,25 @@
+<?php namespace Models;
+class Resources {
+	public static function all($conn) {
+		if (!($stmt = $conn->prepare("SELECT `host`, `cpu_cores`, `memory`, `load1`, `load5`, `load15`, `used_memory`, `last_updated` FROM `resources` WHERE `group`='mslab' ORDER BY `order`"))) {
+			throw new Exception("stmt error");
+		}
+		$stmt->execute();
+		$stmt->bind_result($host, $cpu_cores, $memory, $load1, $load5, $load15, $used_memory, $last_updated);
+		$data = array();
+		while ($stmt->fetch()) {
+			$data[] = array(
+				'host'=> $host,
+				'cpu_cores'=> $cpu_cores,
+				'memory' => (int)$memory,
+				'load1'=> (double)$load1,
+				'load5'=> (double)$load5,
+				'load15'=> (double)$load15,
+				'used_memory' => (int)$used_memory,
+				'last_updated'=> $last_updated
+			);
+		}
+		$stmt->close();
+		return $data;
+	}
+}
