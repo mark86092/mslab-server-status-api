3b8ae4bd76246d798923e71d996487a73d1651f3
Yen-chi Chen <mark86092@gmail.com>
2015-04-23 00:38:55 +0800

    fix CORS typo

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ $route->get('#^/$#', function() {
 		}
 		$stmt->close();
 		
-		header("Access-Control-Allow-Orgin: *");
+		header("Access-Control-Allow-Origin: *");
 		header("Access-Control-Allow-Methods: *");
 		header("Content-Type: application/json");
 
